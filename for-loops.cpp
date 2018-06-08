0964864f83b9bdb46b7a11d520252ab820142c48
Manav Bokinala <manav.bokinala@gmail.com>
2018-06-07 17:20:14 -0700

    Finished for loops lecture

--- for-loops.cpp
+++ for-loops.cpp
@@ -4,10 +4,21 @@ using namespace std;
 
 main() {
 
-/*
-    for(initialization; condition; inc/dec) {
-            instruction repeat
+    /*
+        for(initialization; condition; inc/dec) {
+                instruction to repeat [...]
+        }
+    */
+
+    /*for(int i = 0; i < 10; i++) {
+        cout << i << endl;
+    } */
+
+    int array[100];
+
+    for(int i = 0; i < 100; i++) {
+        array[i] = i;
+        cout << array[i] << endl;
     }
-*/
 
 }
